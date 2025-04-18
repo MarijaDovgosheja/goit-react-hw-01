fb913aed13a51272022ec362f5662925dd180f6e
MaryD <holetsmasha@ukr.net>
2025-04-18 22:20:22 +0300

    homework-1-react

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import css from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={css.card}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p className={`${isOnline ? css.online : css.offline}`}>
+        {isOnline && "Online"}
+        {!isOnline && "Offline"}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
