fa11fd7c58fff9577399954bb122dd04e8c36287
MaryD <holetsmasha@ukr.net>
2025-04-19 22:10:21 +0300

    add styles transaction

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import css from "./FriendListItem.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
@@ -5,12 +6,22 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
     <div className={css.card}>
       <img src={avatar} alt="Avatar" width="48" />
       <p className={css.name}>{name}</p>
-      <p className={`${isOnline ? css.online : css.offline}`}>
-        {isOnline && "Online"}
-        {!isOnline && "Offline"}
+      <p
+        className={clsx(
+          css.status,
+          isOnline === true && css.online,
+          isOnline === false && css.offline
+        )}
+      >
+        {isOnline ? "Online" : "Offline"}
       </p>
     </div>
   );
 };
 
 export default FriendListItem;
+{
+  /* <p className={`${isOnline ? css.online : css.offline}`}>
+        {isOnline && "Online"}
+        {!isOnline && "Offline"} */
+}
